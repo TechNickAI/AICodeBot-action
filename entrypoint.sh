33ac1a40fa0da0272a2aa81234fb11cdcdac0631
Nick Sullivan <nick@sullivanflock.com>
2023-07-08 11:01:48 -0700

    Refactor action.yml and entrypoint.sh for clarity
    In this commit, the action.yml and entrypoint.sh files have been refactored for improved readability and clarity. The changes in action.yml primarily involve replacing single quotes with double quotes for consistency.
    
    In entrypoint.sh, the error message for a missing OpenAI API key has been updated to reference the correct environment variable name. Additionally, the script now echoes the first five characters of the OpenAI API key and the GitHub SHA upon execution, providing more transparency into the script's operation.
    
    The exportation of several environment variables has been removed, presumably because they are no longer necessary. This simplifies the script and reduces potential points of failure.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,17 +3,15 @@
 set -eu
 
 # Check if required inputs are set
-if [[ -z "${INPUT_OPENAI_API_KEY}" ]]; then
-    echo "Error: The OPENAI_API_KEY is not set. Please set it as an environment variable."
+if [[ -z "${INPUT_OPENAI_API_KEY:-}" ]]; then
+    echo "Error: The INPUT_OPENAI_API_KEY is not set. Please set it as an environment variable."
     exit 1
 fi
 
-# Export inputs as environment variables
+echo "Using OpenAI API key: ${OPENAI_API_KEY:0:5}..."
 export OPENAI_API_KEY=${INPUT_OPENAI_API_KEY}
-export COMMENT_SEVERITY_LEVEL=${INPUT_COMMENT_SEVERITY_LEVEL:-info}
-export FAIL_ON_ERROR=${INPUT_FAIL_ON_ERROR:-false}
-export ENABLE_GIF_REACTIONS=${INPUT_ENABLE_GIF_REACTIONS:-false}
 
 # Run aicodebot in the context of the repository
+echo "Running aicodebot in ${GITHUB_WORKSPACE} on sha ${GITHUB_SHA:0:8}"
 cd ${GITHUB_WORKSPACE}
 aicodebot review -c ${GITHUB_SHA}
