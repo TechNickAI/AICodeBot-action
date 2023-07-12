4063175069d28f5494c68d84d5f876cf345b3332
Nick Sullivan <nick@sullivanflock.com>
2023-07-12 15:28:53 -0700

    Integrate PyGithub and add comments to github commits
    The Dockerfile has been updated to include the PyGithub package, expanding the capabilities of the aicodebot_action.py script. The script now utilizes the Github API to interact with commits, providing reactions and comments based on the review status. This enhancement increases the efficiency and interactivity of the code review process.

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,8 @@ FROM python:3.11-slim
 RUN apt-get update && apt-get install -y git jq
 
 RUN pip install aicodebot
+# Additional dependencies for aicodebot_action
+RUN pip install PyGithub
 
 COPY aicodebot_action.py /aicodebot_action.py
 
--- aicodebot_action.py
+++ aicodebot_action.py
@@ -1,12 +1,13 @@
+#!/usr/bin/env python3
 import json
 import os
 import subprocess
 import sys
 
-from rich import Markdown
+from github import Github
 
 # Check if required inputs are set
-openai_api_key = os.getenv('INPUT_OPENAI_API_KEY')
+openai_api_key = os.getenv("INPUT_OPENAI_API_KEY")
 if not openai_api_key:
     print("🛑 The OpenAI API Key is not set. This key is REQUIRED for the AICodeBot.")
     print("You can get one for free at https://platform.openai.com/account/api-keys")
@@ -17,32 +18,60 @@ if not openai_api_key:
     sys.exit(1)
 
 # Set up the git configuration. Allow the user to override the safe directory
-subprocess.run(['git', 'config', '--global', '--add', 'safe.directory', '/github/workspace'])
+subprocess.run(
+    ["git", "config", "--global", "--add", "safe.directory", "/github/workspace"]
+)
+
+# ---------------------------------------------------------------------------- #
+#                              Run the code review                             #
+# ---------------------------------------------------------------------------- #
 
 # Set up the aicodebot configuration from the OPENAI_API_KEY
-subprocess.run(['aicodebot', '-V'])
-subprocess.run(['aicodebot', 'configure'])
+subprocess.run(["aicodebot", "-V"])
+subprocess.run(["aicodebot", "configure", "--openai-api-key", openai_api_key])
 
 # Run a code review on the current commit
-review_output = subprocess.run(['aicodebot', 'review', '-c', os.getenv('GITHUB_SHA'), '--output-format=json'], capture_output=True, text=True)
+review_output = subprocess.run(
+    ["aicodebot", "review", "-c", os.getenv("GITHUB_SHA"), "--output-format=json"],
+    capture_output=True,
+    text=True,
+)
 review_output = json.loads(review_output.stdout)
-review_status = review_output['review_status']
-review_comments = review_output['review_comments']
+review_status = review_output["review_status"]
+review_comments = review_output["review_comments"]
 
 # Magic to set the output variables for github workflows
-with open(os.getenv('GITHUB_STATE'), 'a') as f:
+with open(os.getenv("GITHUB_STATE"), "a") as f:
     f.write(f"{review_status}={review_status}\n")
 
+# ---------------------------------------------------------------------------- #
+#                           Set up the github client                           #
+# ---------------------------------------------------------------------------- #
+g = Github(os.getenv("GITHUB_TOKEN"))
+
+# Get the repository
+repo = g.get_repo(os.getenv("GITHUB_REPOSITORY"))
+
+# Get the commit
+commit = repo.get_commit(os.getenv("GITHUB_SHA"))
+
+
+# ---------------------------------------------------------------------------- #
+#                         Leave a comment on the commit                        #
+# ---------------------------------------------------------------------------- #
+
+
 if review_status == "PASSED":
-    # TOOD: Add thumbs up reaction to the commit
+    commit.create_comment_reaction("+1")
     print("👍 Code review passed!")
-    print(Markdown(review_comments))
 elif review_status == "FAILED":
-    # TODO: Leave a comment with the review_comments
+    commit.create_comment_reaction("-1")
     print("🛑 Code review failed!")
-    print(Markdown(review_comments))
     sys.exit(1)
 elif review_status == "COMMENTS":
-    print(f"Comments: {review_comments}")
-    print(Markdown(review_comments))
+    print("🤔 Code review has comments.")
     sys.exit(0)
+
+if review_comments:
+    commit.create_comment(review_comments)
+    print(f"Comments: {review_comments}")
